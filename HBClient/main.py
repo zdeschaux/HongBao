ae7c3aa3ed1dbfb823fa1f10cc99607c4302bd6b
Zachary Deschaux <zjod95@gmail.com>
2018-07-09 00:56:23 +0800

    Basis of connection between client and server established and tested

--- HBClient/main.py
+++ HBClient/main.py
@@ -1,15 +1,77 @@
+import sys
+import socket
+from threading import *
 from PyQt4 import QtCore, QtGui
+
+from hongbao import Ui_MainWindow, _translate
+import res_rc
 
 class RCDSTool(QtGui.QMainWindow):
     def __init__(self, parent=None):
         super(RCDSTool, self).__init__(parent)
 
-    # Set up UI
-    self.ui = Ui_MainWindow()
-    self.setWindowFlags(self.windowFlags() |
-                          QtCore.Qt.WindowSystemMenuHint |
-                          QtCore.Qt.WindowMinMaxButtonsHint)
-    self.ui.setupUi(self)
+        # Set up UI
+        self.ui = Ui_MainWindow()
+        self.setWindowFlags(self.windowFlags() |
+                              QtCore.Qt.WindowSystemMenuHint |
+                              QtCore.Qt.WindowMinMaxButtonsHint)
+        self.ui.setupUi(self)
+
+        self.conn = None
+        self.client = ''
+        self.host = ''
+        self.port = 8888
+
+        self.ui.pushButtonConnect.clicked.connect(self.Connect)
+        self.ui.pushButtonPrepare.clicked.connect(self.Prepare)
+        self.ui.hongbaoimg.linkActivated.connect(self.Collect)
+        self.ui.pushButtonLastFive.clicked.connect(self.LastFive)
+
+    def Connect(self):
+        ips = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if ip.startswith('192.168.')]
+        ips.sort()
+        for ip in ips:
+            if self.host is '':
+                if '192.168.' in ip:
+                    baseip = ip.rsplit('.', 1)[0]
+                    for i in range(1, 256):
+                        host = str(baseip + '.' + str(i))
+                        try:
+                            conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                            conn.settimeout(0.01)
+                            conn.connect((host, self.port))
+                            self.host = host
+                            self.conn = conn
+                            self.conn.send('e'.encode())
+                            print "Connected"
+                            print host
+                            break
+                        except:
+                            pass
+
+    def Prepare(self):
+        pass
+
+    def Collect(self):
+        pass
+
+    def LastFive(self):
+        pass
+
+    def UpdateLog(self, msg):
+        pass
+
+    def UpdateStatus(self, msg):
+        pass
+
+    # Event on closing
+    def closeEvent(self, event):
+
+        if self.conn is not None:
+            self.conn.close()
+
+        # Let the window close
+        event.accept() 
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
--- HBServer/main.py
+++ HBServer/main.py
@@ -1,8 +1,15 @@
+import errno
 import socket
+import select
 from threading import *
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "127.0.0.1"
+ips = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if ip.startswith('192.168.')]
+ips.sort()
+if ips:
+    host = ips[0]
+else:
+    host = '127.0.0.1'
 port = 8888
 print 'Binding to: ' + host + ':' + str(port)
 serversocket.bind((host, port))
@@ -18,12 +25,39 @@ class client(Thread):
 
     def run(self):
         while 1:
-            print('Client sent:', self.sock.recv(1024).decode())
-            self.sock.send(b'Oi you sent something to me')
+            try:
+                ready = select.select([self.sock], [], [], 0.01)
+                if ready[0]:
+                    data = self.sock.recv(4096).decode()
+                if data is not "":
+                    print('Client sent:', data)
+                data = ""
+                self.sock.send(b'Oi you sent something to me')
+            except socket.error as error:
+                if error.errno == errno.WSAECONNRESET:
+                    self.sock.close()
+                    print "Client disconnected: " + address[0]
+                    break
+                else:
+                    raise
 
 serversocket.listen(5)
 print ('Server started and listening')
+canquit = False
 while 1:
-    clientsocket, address = serversocket.accept()
-    clientlist.append(address[0])
-    client(clientsocket, address)
+    ready = select.select([serversocket], [], [], 0.01)
+    if ready[0]:
+        clientsocket, address = serversocket.accept()
+        clientsocket.setblocking(0)
+        clientlist.append(client(clientsocket, address))
+        print "Client connected: " + address[0]
+        canquit = True
+    for client in clientlist:
+        if not client.isAlive():
+            idx = clientlist.index(client)
+            clientlist[idx:idx+1] = []
+            print "Removed client "  + address[0] + " from list"
+    if not clientlist and canquit:
+        break
+
+
