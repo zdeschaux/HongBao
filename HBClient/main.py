a5be1ad283395ed8bf1a61bcefd48479c6afd47b
Zachary Deschaux <zjod95@gmail.com>
2018-07-12 22:50:00 +0800

    Preliminary connection and authentication done

--- HBClient/main.py
+++ HBClient/main.py
@@ -1,7 +1,12 @@
 import sys
 import socket
+import select
+import errno
+import base64
 from threading import *
 from PyQt4 import QtCore, QtGui
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
 
 from hongbao import Ui_MainWindow, _translate
 import res_rc
@@ -17,37 +22,97 @@ class RCDSTool(QtGui.QMainWindow):
                               QtCore.Qt.WindowMinMaxButtonsHint)
         self.ui.setupUi(self)
 
+        # Server communication
         self.conn = None
         self.client = ''
         self.host = ''
+        self.hostpubkey = ''
         self.port = 8888
+        self.cipherwr = None
 
+        # Encryption
+        self.name = ""
+        self.pin = ""
+        self.key = RSA.generate(4096)
+        self.pubkey = self.key.publickey().exportKey()
+        self.cipherrd = PKCS1_OAEP.new(self.key)
+
+        # UI connections
         self.ui.pushButtonConnect.clicked.connect(self.Connect)
         self.ui.pushButtonPrepare.clicked.connect(self.Prepare)
         self.ui.hongbaoimg.linkActivated.connect(self.Collect)
         self.ui.pushButtonLastFive.clicked.connect(self.LastFive)
 
+    # Handle connection to server
     def Connect(self):
+        self.name = str(self.ui.lineEditName.text())
+        self.pin = str(self.ui.lineEditPIN.text())
+
+        # Find a server
         ips = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if ip.startswith('192.168.')]
         ips.sort()
         for ip in ips:
             if self.host is '':
                 if '192.168.' in ip:
                     baseip = ip.rsplit('.', 1)[0]
-                    for i in range(1, 256):
+
+                    # Try full subrange
+                    for i in range(1, 255):
                         host = str(baseip + '.' + str(i))
                         try:
                             conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                             conn.settimeout(0.01)
                             conn.connect((host, self.port))
-                            self.host = host
-                            self.conn = conn
-                            self.conn.send('e'.encode())
-                            print "Connected"
+                            conn.settimeout(None)
+                            print "Found potential to a server"
                             print host
-                            break
+
+                            # On successful connection, check that it can communicate
+                            conn.send(base64.b64encode(str('? ' + self.pubkey)))
+                            print "Sent"
+                            data = ""
+                            ready = select.select([conn], [], [], 5.0)
+                            if ready[0]:
+                                data = base64.b64decode(conn.recv(4096))
+                            print 'Server sent: ' + data
+
+                            # Did the server send the proper catchphrase and key?
+                            if data.startswith('HongBao8888'):
+                                hostpubkey = data.split('HongBao8888 ')[1]
+                                if len(hostpubkey) == 0:
+                                    break
+
+                                # Try sending encrypted login
+                                for i in range(5):
+                                    cipherwr = PKCS1_OAEP.new(RSA.importKey(hostpubkey))
+                                    ciphermsg = str("NAME: " + self.name + " PIN: " + self.pin)
+                                    ciphermsg = base64.b64encode(cipherwr.encrypt(ciphermsg))
+                                    conn.send(ciphermsg)
+                                    ready = select.select([conn], [], [], 5.0)
+                                    if ready[0]:
+                                        data = base64.b64decode(conn.recv(4096))
+                                    print 'Server sent: ' + data
+
+                                    # Server acknowledged, valid
+                                    if data == "ACK":
+                                        print "Connected to a valid server"
+                                        self.client = ip
+                                        self.host = host
+                                        self.conn = conn
+                                        self.hostpubkey = hostpubkey
+                                        self.cipherwr = cipherwr
+                                        break
+
+                                # If it failed to authenticate, try other addresses
+                                if self.host is not '':
+                                    break
+
+                        # If it can't connect, don't care
                         except:
                             pass
+
+    def Disconnect(self):
+        pass
 
     def Prepare(self):
         pass
@@ -62,6 +127,12 @@ class RCDSTool(QtGui.QMainWindow):
         pass
 
     def UpdateStatus(self, msg):
+        pass
+
+    def SendEncrypted(self, msg):
+        pass
+
+    def CheckServer(self):
         pass
 
     # Event on closing
--- HBServer/main.py
+++ HBServer/main.py
@@ -1,7 +1,14 @@
 import errno
 import socket
 import select
+import base64
 from threading import *
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
+
+key = RSA.generate(4096)
+pubkey = key.publickey().exportKey()
+cipherrd = PKCS1_OAEP.new(key)
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ips = [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if ip.startswith('192.168.')]
@@ -16,27 +23,56 @@ serversocket.bind((host, port))
 
 clientlist = []
 
+# Client communication stored in threads
 class client(Thread):
     def __init__(self, socket, address):
         Thread.__init__(self)
         self.sock = socket
         self.addr = address
+        self.cipherwr = None
+        self.pubkey = ""
+        self.name = ""
+        self.pin = ""
         self.start()
 
     def run(self):
         while 1:
             try:
+                data = ""
                 ready = select.select([self.sock], [], [], 0.01)
                 if ready[0]:
-                    data = self.sock.recv(4096).decode()
-                if data is not "":
-                    print('Client sent:', data)
-                data = ""
-                self.sock.send(b'Oi you sent something to me')
+                    data = base64.b64decode(self.sock.recv(4096))
+
+                # Establish a new connection
+                if data is not "" and self.cipherwr == None:
+                    print self.addr[0] + ' sent: ' + data
+                    if data.startswith("?"):
+                        self.pubkey = data.split("? ")[1]
+                        self.cipherwr = PKCS1_OAEP.new(RSA.importKey(self.pubkey))
+                        self.sock.send(base64.b64encode(str('HongBao8888 ' + pubkey)))
+                        print "Sent auth msg and key to " + self.addr[0]
+                    else:
+                        raise socket.error(errno.WSAECONNRESET, "Did not catch on connection")
+
+                # Handle standard requests
+                elif data is not "" and self.cipherwr is not None:
+                    data = cipherrd.decrypt(data)
+
+                    # Client identification
+                    if data.startswith("NAME:"):
+                        print self.addr[0] + ' sent: ' + data
+                        self.name = data.split("NAME: ")[1].split(" PIN:")[0]
+                        self.pin = data.split("PIN: ")[1]
+                        print self.name + " " + self.pin
+                        self.sock.send(base64.b64encode('ACK'))
+                    else:
+                        raise socket.error(errno.WSAECONNRESET, "Did not catch in use")
+
+            # Likely broken connection from client side, close
             except socket.error as error:
                 if error.errno == errno.WSAECONNRESET:
                     self.sock.close()
-                    print "Client disconnected: " + address[0]
+                    print self.addr[0] + " disconnected"
                     break
                 else:
                     raise
@@ -45,18 +81,25 @@ serversocket.listen(5)
 print ('Server started and listening')
 canquit = False
 while 1:
+    # Are there any pending connections?
     ready = select.select([serversocket], [], [], 0.01)
     if ready[0]:
+
+        # Set up new clients waiting
         clientsocket, address = serversocket.accept()
         clientsocket.setblocking(0)
         clientlist.append(client(clientsocket, address))
         print "Client connected: " + address[0]
         canquit = True
+
+    # Have any clients disconnected?
     for client in clientlist:
         if not client.isAlive():
             idx = clientlist.index(client)
             clientlist[idx:idx+1] = []
             print "Removed client "  + address[0] + " from list"
+
+    # Are there any clients?
     if not clientlist and canquit:
         break
 
